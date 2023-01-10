ce9f0e27308aff03bb21b6aa2b0798b254229582
EC2 Default User <ec2-user@ip-172-31-10-249.ap-northeast-1.compute.internal>
2023-01-10 13:20:44 +0000

    viewは記入済み（エラーあり）

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,6 @@
 class Public::HomesController < ApplicationController
+  
+  def top
+  end
+  
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,31 @@
 class Public::RegistrationsController < ApplicationController
+   before_action :configure_sign_up_params, only: [:create]
+  
   def new
   end
 
   def create
   end
+  
+  
+  protected
+  
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+  end
+  
+  def after_sign_in_path_for(resource)
+     public_customers_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+   
+  protected
+  
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name,:first_name,:family_kana,:first_kana,:postal_code,:address,:telephone_number]) 
+  end
+
 end
