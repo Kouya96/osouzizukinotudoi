8333b5a1fb1845cd20f2cbe66c045344176ea474
EC2 Default User <ec2-user@ip-172-31-10-249.ap-northeast-1.compute.internal>
2023-01-10 14:19:58 +0000

    adminのroutesが消えたので修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,39 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+
+  namespace :admin do
+    get 'homes/top'
+  end
+
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/create'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
+
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+
   namespace :public do
     get 'favorites/create'
     get 'favorites/destroy'
