09616b1daa2941c7febbfdc378f8d74e6602f23a
EC2 Default User <ec2-user@ip-172-31-10-249.ap-northeast-1.compute.internal>
2023-01-12 11:22:22 +0000

    Admin作り直し、routes.rbをresourcesに変更完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,79 +1,95 @@
 Rails.application.routes.draw do
 
   devise_for :admins
-  
+  #devise_for :admins
+
   root to: "public/homes#top"
   get 'about' => 'public/homes#about', as: 'about'
 
   devise_for :users
 
   namespace :admin do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-
-  namespace :admin do
     get 'homes/top'
   end
 
   namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
+    resources :items
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
 
   namespace :public do
-    get 'favorites/create'
-    get 'favorites/destroy'
+    resources :favorites, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+    resources :customers, only: [:show, :edit, :update, :unsubscride, :withdraw]
+    resources :items, only: [:index, :show, :new, :destroy, :create, :edit, :update, :search]
   end
-  namespace :public do
-    get 'comments/create'
-    get 'comments/destroy'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscride'
-    get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'registrations/new'
-    get 'registrations/create'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-    get 'items/new'
-    get 'items/destroy'
-    get 'items/create'
-    get 'items/edit'
-    get 'items/update'
-    get 'items/search'
-  end
+
+#実装終了するまで一応残しておく※終了後削除する
+
+  # #namespace :admin do
+  #   get 'sessions/new'
+  #   get 'sessions/create'
+  #   get 'sessions/destroy'
+  # end
+
+  #   # get 'items/index'
+  #   # get 'items/new'
+  #   # get 'items/create'
+  #   # get 'items/show'
+  #   # get 'items/edit'
+  #   # get 'items/update'
+  # end
+
+  # namespace :admin do
+  #   get 'genres/index'
+  #   get 'genres/create'
+  #   get 'genres/edit'
+  #   get 'genres/update'
+  # end
+
+  # namespace :admin do
+  #   get 'customers/index'
+  #   get 'customers/show'
+  #   get 'customers/edit'
+  #   get 'customers/update'
+  # end
+
+  # namespace :public do
+  #   get 'favorites/create'
+  #   get 'favorites/destroy'
+  # end
+  # namespace :public do
+  #   get 'comments/create'
+  #   get 'comments/destroy'
+  # end
+  # namespace :public do
+  #   get 'customers/show'
+  #   get 'customers/edit'
+  #   get 'customers/update'
+  #   get 'customers/unsubscride'
+  #   get 'customers/withdraw'
+  # end
+  # namespace :public do
+  #   get 'sessions/new'
+  #   get 'sessions/create'
+  #   get 'sessions/destroy'
+  # end
+  # namespace :public do
+  #   get 'registrations/new'
+  #   get 'registrations/create'
+  # end
+  # namespace :public do
+  #   get 'items/index'
+  #   get 'items/show'
+  #   get 'items/new'
+  #   get 'items/destroy'
+  #   get 'items/create'
+  #   get 'items/edit'
+  #   get 'items/update'
+  #   get 'items/search'
+  # end
   #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_11_113443) do
+ActiveRecord::Schema.define(version: 2023_01_12_110326) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -19,9 +19,12 @@ ActiveRecord::Schema.define(version: 2023_01_11_113443) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.string "name", null: false
-    t.text "introduction"
     t.datetime "remember_created_at"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.text "introduction"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -64,7 +67,10 @@ ActiveRecord::Schema.define(version: 2023_01_11_113443) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
