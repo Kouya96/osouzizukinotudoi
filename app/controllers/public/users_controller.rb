db51f5ec088e6ba3d4bea3695caab0de3e4bc0f2
EC2 Default User <ec2-user@ip-172-31-10-249.ap-northeast-1.compute.internal>
2023-01-15 13:07:33 +0000

    ユーザー情報編集画面完了

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -30,4 +30,9 @@ class Public::UsersController < ApplicationController
         flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
         redirect_to root_path
   end
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :introduction, :id)
+  end
+
 end
