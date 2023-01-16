4e6973a94a6975ec8d94e0bf6fb4011815e3bc5b
EC2 Default User <ec2-user@ip-172-31-10-249.ap-northeast-1.compute.internal>
2023-01-16 13:11:22 +0000

    コメント機能作成

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,7 +1,25 @@
 class Public::CommentsController < ApplicationController
+ 
   def create
+    @item = Item.find(params[:item_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.item_id = @item.id
+    @comment.save
+
+    redirect_to request.referer
   end
 
+
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to request.referer
+  end
+
+
+  private
+  def comment_params
+    params.permit(:comment)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #has_many
-  has_many :items
+  has_many :comments, dependent: :destroy
+  has_many :items, dependent: :destroy
   has_one_attached :profile_image
 
   def get_profile_image
