f573079b2a4a8d430b79e9d6488865d1ce87cf1d
EC2 Default User <ec2-user@ip-172-31-10-249.ap-northeast-1.compute.internal>
2023-01-24 09:49:34 +0000

    インデント整理,

--- app/controllers/public/users/sessions_controller.rb
+++ app/controllers/public/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :active_user!, only: [:create]
 
   #ゲストログイン
   def guest_sign_in
@@ -9,11 +10,6 @@ class Public::Users::SessionsController < Devise::SessionsController
     sign_in user
     redirect_to public_items_path, notice: 'ゲストユーザーでログインしました。'
   end
-end
-
-def new
-end
-
 
   # GET /resource/sign_in
   # def new
@@ -37,3 +33,14 @@ end
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
+  private
+
+  def active_user!
+    user = User.find_by(email: params[:user][:email]) # フォームに入力されたメールアドレスでユーザーを取得
+    return if user.nil? # ユーザーが取得できなければなにもしない
+    return unless user.valid_password?(params[:user][:password]) # フォームに入力されたパスワードが間違っていればなにもしない
+    unless user.is_active?
+      redirect_to new_user_session_path, alert: "退会したユーザーはログイン出来ません"
+    end
+  end
+end
